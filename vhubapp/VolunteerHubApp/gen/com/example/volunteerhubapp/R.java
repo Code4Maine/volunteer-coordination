df989ca6fe2350dfa3db6fa417df92a7e48cc06b
Zachery <zacheryschiller@gmail.com>
2014-06-01 02:57:06 -0400

    Updating home screen for Android app.

--- vhubapp/VolunteerHubApp/gen/com/example/volunteerhubapp/R.java
+++ vhubapp/VolunteerHubApp/gen/com/example/volunteerhubapp/R.java
@@ -1092,12 +1092,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int button1=0x7f05003f;
         public static final int button2=0x7f050040;
+        public static final int button3=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1142,7 +1143,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f050041;
+        public static final int textView1=0x7f050042;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
