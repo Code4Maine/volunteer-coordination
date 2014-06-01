25e672bc1264a8efda7ef2b541e26740cffa01c2
Colin Powell <colin.powell@gmail.com>
2014-06-01 04:09:08 -0400

    JSON urls should use dot paths.

--- volunteerhub/apps/projects/urls.py
+++ volunteerhub/apps/projects/urls.py
@@ -6,27 +6,27 @@ from .views import (OpportunityDetailView, ProjectListView,
 # custom views
 urlpatterns = patterns(
     '',
-    url(r'^(?P<slug>[-\w]+)/$',
+    url(r'^projects/',
         view=ProjectListView.as_view(),
         name="project-list"),
 
-    url(r'^(?P<slug>[-\w]+).json$',
+    url(r'^projects.json',
         view=ProjectListJSONView.as_view(),
         name="project-list-json"),
 
-    url(r'^(?P<slug>[-\w]+)/opportunities/$',
+    url(r'^(?P<slug>[-\w]+)/opportunities/',
         view=ProjectDetailView.as_view(),
         name="project-detail"),
 
-    url(r'^(?P<slug>[-\w]+)/opportunities.json$',
+    url(r'^(?P<slug>[-\w]+)/opportunities.json',
         view=ProjectDetailJSONView.as_view(),
         name="project-detail-json"),
 
-    url(r'^(?P<project_slug>[-\w]+)/opportunities/(?P<slug>[-\w]+)/$',
+    url(r'^(?P<project_slug>[-\w]+)/opportunities/(?P<slug>[-\w]+)/',
         view=OpportunityDetailView.as_view(),
         name="opportunity-detail"),
 
-    url(r'^(?P<project_slug>[-\w]+)/opportunities/(?P<slug>[-\w]+).json$',
+    url(r'^(?P<project_slug>[-\w]+)/opportunities/(?P<slug>[-\w]+).json',
         view=OpportunityDetailJSONView.as_view(),
         name="opportunity-detail-json"),
 )
